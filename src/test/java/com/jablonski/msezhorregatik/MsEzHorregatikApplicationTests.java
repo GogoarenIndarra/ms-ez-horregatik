035c1a89372d52ad3730f1506c8ea1af22e7b979
jablonski.pio <piotr.jablonski@bcf-software.com>
2022-09-01 19:43:31 +0200

    init commit

---
+++ src/test/java/com/jablonski/msezhorregatik/MsEzHorregatikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jablonski.msezhorregatik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEzHorregatikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
